6fbe03d989af09516b1506e0b5a25ca847128759
Matthew Blott <matthew.blott@gmail>
2019-09-26 11:05:41 +0100

    Upgraded to .net 3.0

---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace turbolinks_example
+{
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc()
+        .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+      services
+        .AddControllersWithViews()
+        .AddRazorRuntimeCompilation();
+      
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseStaticFiles();
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute("default", "{controller}/{action}/{id?}", 
+          defaults: new { controller = "Home", action = "Index" });
+        
+        endpoints.MapRazorPages();
+        
+      });
+
+    }
+    
+    public static void Main(string[] args) =>
+      WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
+
+  }
+  
+}
